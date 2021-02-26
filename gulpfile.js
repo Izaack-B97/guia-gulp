4df29b08bc0d5294bcb517d03df36a427ef97333
Isaac Bustamante <isaac@gengen-mac.local>
2021-02-26 14:56:30 -0700

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const { 
+    watch, 
+    series, 
+    parallel, 
+    src, 
+    dest 
+} = require('gulp');
+
+// Tarea Hola Mundo
+function holaMundo ( cb ) {
+    console.log(`Hola mundo ${ Date.now() }`);
+    cb();
+}
+
+// Tarea Adios Mundo
+function adiosMundo( cb ) {
+    console.log(`Adios mundo ${ Date.now() }`);
+    cb();
+} 
+
+function watch_css( cb ) {
+    watch('css/*css', holaMundo);
+    cb();
+}
+
+// Declaro las tareas publicas con exports
+exports.holaMundo = holaMundo;
+exports.adiosMundo = adiosMundo;
+exports.default = holaMundo;
+exports.serie = series( adiosMundo, holaMundo );
+exports.paralelo = parallel( adiosMundo, holaMundo );
+
+exports.pipeline = function () {
+    return src('css/*.css').pipe( dest('dist/') );
+}
